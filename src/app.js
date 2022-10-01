7103a00351375f046ca10b911074f3471d45e240
Raul Andrial <rauljandrial@gmail.com>
2022-10-01 07:45:04 +0000

    Initial commit - project completed

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let allCombos = genAllCombos(
+    genAllCombos(genAllCombos(opts[0], opts[1]), opts[2]),
+    opts[3]
+  );
+
+  for (let combo of allCombos) console.log(combo);
 };
+
+let pronouns = ["the", "our"];
+let adjs = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+let exts = [".com", ".us", ".net", ".io"];
+
+const opts = [pronouns, adjs, nouns, exts];
+
+function genAllCombos(arr1, arr2) {
+  let combos = [];
+
+  for (let x of arr1) {
+    combos = [...combos, ...arr2.map(y => `${x}${y}`)];
+  }
+
+  return combos;
+}
